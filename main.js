37dc55995f3f46667cd74362d5886ba55ee73f5b
TC McCarthy <tc.mccarthy@newsday.com>
2021-02-16 20:19:01 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function age_in_years(years){
+	return years * 12;
+}
+
+
+console.log(`My dad, Tim, is 56 years old (${age_in_years(56)} months)`);
+console.log(`My mom, Maria, is 56 years old (${age_in_years(56)} months)`);
+console.log(`My sister, Ashley, is 28 years old (${age_in_years(28)} months)`);
